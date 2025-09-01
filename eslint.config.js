a9bde1390943d79b2cd6e2fdf7aac1523bec4fcb
ferzik85 <sersid@bk.ru>
2025-09-01 13:55:26 +0200

    save

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from 'eslint-config-prettier'
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
 import { globalIgnores } from "eslint/config";
@@ -15,7 +16,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
-      'eslint-config-prettier',
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
