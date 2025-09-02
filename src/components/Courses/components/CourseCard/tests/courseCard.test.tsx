e2e5fb871f485781361325a82ad88f8b8c489452
ferzik85 <sersid@bk.ru>
2025-09-02 17:58:00 +0200

    add tests

---
+++ src/components/Courses/components/CourseCard/tests/courseCard.test.tsx
@@ -0,0 +1,43 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { screen } from '@testing-library/react';
+import { testRender } from '../../../../../tests';
+import CourseCard from '../CourseCard';
+
+const props = {
+  title: 'dummy-title',
+  description: 'dummy-description',
+  creationDate: '24/11/2023',
+  authors: ['author1', 'author2'],
+  duration: 120,
+  id: '',
+};
+
+beforeEach(() => {
+	testRender(<CourseCard {...props} />);
+});
+
+describe('CourseCard tests', () => {
+	test('CourseCard should display title', () => {
+		expect(screen.getByText(props.title)).toBeInTheDocument();
+	});
+
+	test('CourseCard should display description', () => {
+		expect(screen.getByText(props.description)).toBeInTheDocument();
+	});
+
+	test('CourseCard should display creation date in the correct format', () => {
+		const expectedCreationDate = '24.11.2023';
+		expect(screen.getByText(expectedCreationDate)).toBeInTheDocument();
+	});
+
+	test('CourseCard should display authors list', () => {
+		const expectedAuthorsList = 'author1, author2';
+		expect(screen.getByText(expectedAuthorsList)).toBeInTheDocument();
+	});
+
+	test('CourseCard should display duration in the correct format', () => {
+		const expectedDuration = '02:00 hours';
+		expect(screen.getByText(expectedDuration)).toBeInTheDocument();
+	});
+});
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,8 +2,7 @@ import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
-// runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
-	console.log('clean up called');
 	cleanup();
+	console.log('clean up finished');
 });
